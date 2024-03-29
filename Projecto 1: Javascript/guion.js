e599d22ad1214697b50f6eed78a7a2c37eefdba8
migueacastro <miguelcastropadron@gmail.com>
2024-03-29 15:11:35 -0400

    error actualizando la tabla

--- Projecto 1: Javascript/guion.js
+++ Projecto 1: Javascript/guion.js
@@ -1,7 +1,7 @@
 class menu {
     // id, id del boton que invoca al menu, objeto del menu anterior, id del boto para volver a ese menu anterior, funcion ejecutada al mostrar
     // Los 4 atributos después de id son opcionales
-    constructor(id, callerId=null, menuAnterior=null, backId=null, specialFunction=null) {
+    constructor(id, callerId = null, menuAnterior = null, backId = null, specialFunction = null) {
         this.id = id;
         this.menuAnterior = menuAnterior;
         this.elemento = document.getElementById(this.id);
@@ -13,7 +13,7 @@ class menu {
         // no sea nulo.
         if (callerId) {
             this.caller = document.getElementById(callerId);
-            this.caller.addEventListener('click', () => { this.mostrar();});
+            this.caller.addEventListener('click', () => { this.mostrar(); });
         }
 
         // Asignar escucha a un posible boton de volver
@@ -25,19 +25,19 @@ class menu {
         }
 
         // Por defecto, el elemento es ocultado
-        this.elemento.setAttribute('hidden','');
+        this.elemento.setAttribute('hidden', '');
     }
     ocultar() {
-        this.elemento.setAttribute('hidden','');
+        this.elemento.setAttribute('hidden', '');
     }
 
     // Ocultara todas las secciones y mostrará el menú actual
     mostrar() {
         if (this.specialFunction) {
-            this.specialFunction(); 
+            this.specialFunction();
         }
-        
-        document.querySelectorAll('.seccion').forEach( elemento => {
+
+        document.querySelectorAll('.seccion').forEach(elemento => {
             elemento.setAttribute('hidden', '');
         })
         this.elemento.removeAttribute('hidden');
@@ -50,44 +50,77 @@ class menu {
 // Objeto del cliente actual: Acumula cantidades y montos
 clienteActual = [
     {
-        'letra':'A',
-        'cantidad':0.0,
-        'acumulado':0.0,
+        'letra': 'A',
+        'cantidad': 0.0,
+        'acumulado': 0.0,
     },
     {
-        'letra':'B',
-        'cantidad':0.0,
-        'acumulado':0.0,
+        'letra': 'B',
+        'cantidad': 0.0,
+        'acumulado': 0.0,
     },
     {
-        'letra':'C',
-        'cantidad':0.0,
-        'acumulado':0.0,
+        'letra': 'C',
+        'cantidad': 0.0,
+        'acumulado': 0.0,
     }
 ]
 
 
 // Lista de objetos que usaremos para seleccionar
 listaCategorias = [
-    {'letra':'A','nombre':'cigarrilos y Bebidas Alcohólicas', 'impuestos':20/100, 'ventas':0, 'acumulado':0},
-    {'letra':'B','nombre':'enlatados y Carnes', 'impuestos':10/100, 'ventas':0, 'acumulado':0},
-    {'letra':'C','nombre':'arroz, Azúcar y Huevos', 'impuestos':10/100, 'ventas':0, 'acumulado':0},
+    { 'letra': 'A', 'nombre': 'cigarrilos y Bebidas Alcohólicas', 'impuestos': 26 / 100, 'ventas': 0, 'acumulado': 0 },
+    { 'letra': 'B', 'nombre': 'enlatados y Carnes', 'impuestos': 16 / 100, 'ventas': 0, 'acumulado': 0 },
+    { 'letra': 'C', 'nombre': 'arroz, Azúcar y Huevos', 'impuestos': 0, 'ventas': 0, 'acumulado': 0 },
 ]
+
+function actualizarTotal() {
+    clienteActual.forEach(objeto => {
+        objeto.acumulado = 0.0;
+        objeto.cantidad = 0;
+    })
+    tabla = document.getElementById('tablaProductos');
+    let total=0, productos=0;
+    Array.from.tabla.rows.forEach((fila, index) => {
+        if (index != 0) {
+            let categoria = fila.childNodes[4].childNodes[0].innerText;
+            let cantidad = fila.childNodes[1].childNodes[0].value;
+            if (!(cantidad == "")) {
+                subtotal = subtotal.replace("Bs.", "");
+                total += parseFloat(subtotal);
+                productos += parseInt(cantidad);
+                clienteActual.forEach(objeto => {
+                if (objeto.letra == categoria) {
+                    objeto.acumulado += subtotal;
+                    objeto.cantidad += cantidad;
+                }
+            })
+            }   
+        }
+        
+        
+    })
+    // Actualizar objeto de usuario
+    
+    document.getElementById("total").innerText = "Total: "+total.toFixed(2)+"Bs.";
+    document.getElementById("productos").innerText = "Productos: "+productos;
+}
 
 function eliminarFila(elemento) {
     // Boton --> Celda --> Fila
     let fila = elemento.parentNode.parentNode;
     // Acceer a la tabla y eliminar el hijo que sea esta fila
     fila.parentNode.removeChild(fila);
+    actualizarTotal();
 
 }
 function agregarFila() {
     tabla = document.getElementById('tablaProductos');
     longitudTabla = tabla.rows.length;
-    for (let i = 0; i <  longitudTabla; i++) {
+    for (let i = 0; i < longitudTabla; i++) {
         // Agregar elemento hijo antes del footer de la tabla
         if (i == longitudTabla - 1) {
-            let fila = tabla.insertRow(i+1);
+            let fila = tabla.insertRow(i + 1);
 
             // Crear celda con el input precio
             let celda1 = fila.insertCell(0);
@@ -95,7 +128,7 @@ function agregarFila() {
             precio.min = 0;
             precio.type = "number";
             precio.id = "precio" + i;
-            celda1.appendChild(precio);
+            
 
             // Crear celda con el input cantidad
             let celda2 = fila.insertCell(1);
@@ -103,7 +136,7 @@ function agregarFila() {
             cantidad.min = 1;
             cantidad.type = "number";
             cantidad.id = "cantidad" + i;
-            celda2.appendChild(cantidad);
+
 
             // Crear celda de categoria
             let celda3 = fila.insertCell(2);
@@ -116,44 +149,118 @@ function agregarFila() {
             `);
             // Crear un ciclo que asigne las opciones disponibles
             categoria.id = "categoria" + i;
-            celda3.appendChild(categoria);
+
 
             // Crear celda de impuestos
             let celda4 = fila.insertCell(3);
             celda4.id = "impuestos" + i;
-            celda4.appendChild(document.createTextNode("0Bs."));
+
 
             // Crear celda de subtotal
             let celda5 = fila.insertCell(4);
             celda5.id = "subtotal" + i;
-            celda5.appendChild(document.createTextNode("0Bs."));  
-            
+
+
             // Crear boton de eliminar 
             let celda6 = fila.insertCell(5);
             let botonEliminar = document.createElement("button");
             botonEliminar.innerText = "X"
-            botonEliminar.id = "eliminar"+i;
-            botonEliminar.addEventListener('click', ()=> {eliminarFila(botonEliminar);})
+            botonEliminar.id = "eliminar" + i;
+            botonEliminar.addEventListener('click', () => { eliminarFila(botonEliminar); })
+
+            // Agregar escuchadores de eventos para actualizar impuestos y subtotal
+            let actualizarDatosFila = () => {
+                // Si cualquiera de los dos inputs no son numeros, verificar si no es numero y en dado caso
+                // Establecer el dato como nulo, y luego terminar la funcion 
+                if (isNaN(precio.value) || isNaN(cantidad.value)) {
+                    precio.value = isNaN(precio.value) ? null : precio.value;
+                    cantidad.value = isNaN(cantidad.value) ? null : cantidad.value;
+
+                    // Reestablecer los valores de Impuestos y Subtotal
+                    celda4.innerText = "0Bs.";
+                    celda5.innerText = "0Bs.";
+                    actualizarTotal();
+                    return;
+                }
+                else if ((precio.value == "") || (cantidad.value == "")) {
+                    precio.value = (precio.value == "") ? null : precio.value;
+                    cantidad.value = (cantidad.value == "") ? null : cantidad.value;
+
+                    // Reestablecer los valores de Impuestos y Subtotal
+                    celda4.innerText = "0Bs.";
+                    celda5.innerText = "0Bs.";
+                    actualizarTotal();
+                    return;
+                }
+                // Iterar en las categorias hasta que la letra coincida con el valor de 
+                // la categoria seleccionada
+                let impuestos;
+                listaCategorias.forEach(elemento => {
+                    if (elemento.letra == categoria.value) {
+                        impuestos = elemento.impuestos
+                    }
+                });
+                subtotal = parseFloat(precio.value) * parseFloat(cantidad.value);
+                subtotal += parseFloat(subtotal) * parseFloat(impuestos);
+                celda4.innerText = parseFloat(impuestos * subtotal).toFixed(2) + "Bs.";
+                celda5.innerText = subtotal.toFixed(2) + "Bs.";
+
+                actualizarTotal();
+            }
+
+            // Agregar escuchadores de eventos a los elementos de la fila
+            precio.addEventListener('keyup', () => {
+                actualizarDatosFila();
+            })
+            cantidad.addEventListener('keyup', () => {
+                actualizarDatosFila();
+            });
+            categoria.addEventListener('change', () => {
+                actualizarDatosFila();
+            })
+
+            // Agregar elemetos a la fila
+            celda1.appendChild(precio);
+            celda2.appendChild(cantidad);
+            celda3.appendChild(categoria);
+            celda4.innerText = "0Bs";
+            celda5.innerText = "0Bs";
             celda6.appendChild(botonEliminar);
         }
     }
 }
 
+function pagar() {
+    let pago = document.getElementById("pagarI").value;
+    let total= document.getElementById("total").innerText.replace("Total:","");
+    pago = parseFloat(pago);
+    total = parseFloat(total.replace("Bs",""));
+    let vuelto = ((pago - total).toFixed(2)) + "Bs.";
+    if (pago < total) {
+        alert("Saldo insuficiente.");
+    }
+    else {
+        alert("Vuelto: " + vuelto);
+    }
+    document.getElementById('cerrarCajaB').removeAttribute('hidden');
+}
+
 function reiniciarTabla() {
-    
+
     // Eliminar todas las filas de la tabla de productos
     let tabla = document.getElementById('tablaProductos');
     let longitudTabla = tabla.rows.length;
     for (let i = longitudTabla - 1; i > 0; i--) {
         tabla.deleteRow(i);
     }
+    actualizarTotal();
 }
 
 function iniciar() {
     // Aqui asignare la escucha de eventos a cada boton que quiero que muestre
     // un menu determinado.
 
-    
+
     // Menu abrir caja, cuando aparece, ejecuta la funcion para ocultar el boton "Cerrar caja"
     let menuAbrirCaja = new menu('cajaM', 'cerrarCajaB', null, null, function () {
         document.getElementById('cerrarCajaB').setAttribute('hidden', '');
@@ -163,19 +270,18 @@ function iniciar() {
     // Menu inicio , cuando aparece, ejecuta la funcion para mostrar el boton "Cerrar caja"
     let menuInicio = new menu('inicioM', 'abrirCajaB', menuAbrirCaja, null)
 
-    let menuVenta = new menu ('ventaM', 'nuevaVentaB', menuInicio, 'pagarB');
-    document.getElementById('añadirProducto').addEventListener('click', ()=> {
-            agregarFila();
-        }
+    let menuVenta = new menu('ventaM', 'nuevaVentaB', menuInicio);
+    document.getElementById('añadirProducto').addEventListener('click', () => {
+        agregarFila();
+    }
     );
-    document.getElementById('pagarB').addEventListener('click',  function () {
-        document.getElementById('cerrarCajaB').removeAttribute('hidden');
-        reiniciarTabla();
-    });    
+    document.getElementById('pagarB').addEventListener('click', function () {
+        pagar()
+    });
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
     iniciar();
-    
+
 });
